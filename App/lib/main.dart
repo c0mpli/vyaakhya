32d44197d4f070dac0a47ad3cab831b605f4e89e
compli <shirothegamer69@gmail.com>
2024-07-23 16:00:19 +0530

    idk

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:wifi_iot/wifi_iot.dart';
 import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
 }
-
-const String STA_DEFAULT_SSID = "STA_SSID";
-const String STA_DEFAULT_PASSWORD = "STA_PASSWORD";
-const NetworkSecurity STA_DEFAULT_SECURITY = NetworkSecurity.WPA;
-
-const String AP_DEFAULT_SSID = "AP_SSID";
-const String AP_DEFAULT_PASSWORD = "AP_PASSWORD";
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -38,11 +29,6 @@ class _BleScreenState extends State<BleScreen> {
   List<ScanResult> scanResults = [];
   BluetoothDevice? connectedDevice;
   BluetoothCharacteristic? targetCharacteristic;
-  bool isScanning = false;
-  bool isLoading = false;
-  List<WiFiNetwork?> wifiNetworks = [];
-  TextEditingController ssidController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
 
   @override
   void initState() {
@@ -60,7 +46,6 @@ class _BleScreenState extends State<BleScreen> {
 
     if (status.values.every((status) => status.isGranted)) {
       startScan();
-      scanForWiFi();
     } else {
       // Handle permission denied
       print("Permissions not granted");
@@ -68,10 +53,6 @@ class _BleScreenState extends State<BleScreen> {
   }
 
   void startScan() {
-    setState(() {
-      isScanning = true;
-      scanResults.clear();
-    });
     FlutterBluePlus.startScan(timeout: const Duration(seconds: 15));
     FlutterBluePlus.scanResults.listen((results) {
       setState(() {
@@ -80,31 +61,8 @@ class _BleScreenState extends State<BleScreen> {
             .toList();
       });
     }).onDone(() {
-      setState(() {
-        isScanning = false;
-      });
       FlutterBluePlus.stopScan();
     });
-  }
-
-  void stopScan() {
-    FlutterBluePlus.stopScan();
-    setState(() {
-      isScanning = false;
-    });
-  }
-
-  void toggleScan() {
-    if (isScanning) {
-      stopScan();
-    } else {
-      startScan();
-    }
-  }
-
-  void scanForWiFi() async {
-    wifiNetworks = await WiFiForIoTPlugin.loadWifiList();
-    setState(() {});
   }
 
   void connectToDevice(BluetoothDevice device) async {
@@ -136,9 +94,12 @@ class _BleScreenState extends State<BleScreen> {
         print("Service: ${service.uuid}");
         if (service.uuid.toString() == ServiceUUIDToMatch) {
           print("Found service");
+          //print(service.characteristics);
           for (BluetoothCharacteristic characteristic
               in service.characteristics) {
-            if (characteristic.uuid.toString() == CharacteristicUUIDToMatch) {
+            print(characteristic);
+            if (characteristic.characteristicUuid.toString() ==
+                CharacteristicUUIDToMatch) {
               print("Characteristic: ${characteristic.uuid}");
               if (characteristic.properties.write ||
                   characteristic.properties.writeWithoutResponse) {
@@ -158,16 +119,17 @@ class _BleScreenState extends State<BleScreen> {
   }
 
   void sendMessage() async {
+    print(targetCharacteristic);
     if (targetCharacteristic != null) {
       try {
-        String ssid = ssidController.text;
-        String password = passwordController.text;
-        String message = "Hi,SSID:$ssid,PASSWORD:$password";
-        List<int> value = utf8.encode(message);
-
+        print('Sending message');
+        List<int> value = utf8.encode("Hi");
+        //print(targetCharacteristic);
         if (targetCharacteristic!.properties.write) {
+          print("Writing with response");
           await targetCharacteristic!.write(value, withoutResponse: false);
         } else if (targetCharacteristic!.properties.writeWithoutResponse) {
+          print("Writing without response");
           await targetCharacteristic!.write(value, withoutResponse: true);
         } else {
           print("Characteristic is not writable");
@@ -189,82 +151,32 @@ class _BleScreenState extends State<BleScreen> {
       appBar: AppBar(
         title: const Text('BLE Demo'),
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : connectedDevice == null
-              ? Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: toggleScan,
-                      child: Text(isScanning ? 'Stop' : 'Scan'),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: scanResults.length,
-                        itemBuilder: (context, index) {
-                          ScanResult result = scanResults[index];
-                          return ListTile(
-                            title: Text(result.device.platformName.isEmpty
-                                ? "Unnamed"
-                                : result.device.platformName),
-                            subtitle: Text(result.device.remoteId.toString()),
-                            onTap: () => connectToDevice(result.device),
-                          );
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: ssidController,
-                            decoration: const InputDecoration(
-                              labelText: 'SSID',
-                            ),
-                          ),
-                          TextField(
-                            controller: passwordController,
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
-                              obscureText: true,
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: scanForWiFi,
-                            child: const Text('Scan for Wi-Fi'),
-                          ),
-                          Expanded(
-                            child: ListView.builder(
-                              itemCount: wifiNetworks.length,
-                              itemBuilder: (context, index) {
-                                WiFiNetwork network = wifiNetworks[index];
-                                return ListTile(
-                                  title: Text(network.ssid ?? 'Unnamed'),
-                                  onTap: () {
-                                    ssidController.text = network.ssid ?? '';
-                                  },
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Connected to ${connectedDevice!.platformName}'),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: sendMessage,
-                      child: const Text('Send Hi and Wi-Fi Details'),
-                    ),
-                  ],
+      body: connectedDevice == null
+          ? ListView.builder(
+              itemCount: scanResults.length,
+              itemBuilder: (context, index) {
+                ScanResult result = scanResults[index];
+                return ListTile(
+                  title: Text(result.device.platformName.isEmpty
+                      ? "Unnamed"
+                      : result.device.platformName),
+                  subtitle: Text(result.device.remoteId.toString()),
+                  onTap: () => connectToDevice(result.device),
+                );
+              },
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Connected to ${connectedDevice!.platformName}'),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: sendMessage,
+                  child: const Text('Send Hi'),
                 ),
+              ],
+            ),
     );
   }
 }
